a82369da3848b9cad412c3528ead3985ed30ac0c
Luis Alberto Luft <luberluft@gmail.com>
2020-02-22 15:16:00 -0400

    Add barry ide-helper for better laravel autocompletion

--- app/Movie.php
+++ app/Movie.php
@@ -4,6 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Movie
+ *
+ * @property int $id
+ * @property int $profile_id
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Movie newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Movie newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Movie query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Movie whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Movie whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Movie whereProfileId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Movie whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Movie whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Movie extends Model
 {
     //
--- app/Profile.php
+++ app/Profile.php
@@ -4,6 +4,26 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Profile
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Movie[] $movies
+ * @property-read int|null $movies_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Profile extends Model
 {
     public function movies()
