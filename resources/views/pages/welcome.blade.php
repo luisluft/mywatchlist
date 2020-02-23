f6df2542256161a3f5694c6480db5151b97d63fc
Luis Alberto Luft <luberluft@gmail.com>
2020-02-23 16:41:14 -0400

    Add tmdb logo to welcome page

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -85,15 +85,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <img src="{{asset('storage/tmdb-logo.png')}}" alt="Flowers in Chania">
                 </div>
+
             </div>
         </div>
     </body>
