0c3b7ac47b9b0b59d4ba9d671293a96443bedcc1
Luis Alberto Luft <luberluft@gmail.com>
2020-02-24 11:11:30 -0400

    Add show search results feature

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -5,8 +5,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <!-- Button to Open the Modal -->
-            <input v-model=queryString type="text">
-            <button id="searchTMDB" type="button" class="btn btn-primary" data-toggle="modal" data-target="newProfile" v-on:click="searchTMDB" autofocus>
+            <input v-model=queryString type="text" class="col-sm-8">
+            <button id="searchTMDB" type="button" class="btn btn-primary col-sm-2" data-toggle="modal" data-target="newProfile" v-on:click="searchTMDB" autofocus>
                 Search
             </button>
 
@@ -20,18 +20,31 @@
                 </div>
         @endforeach
 
-        <!-- Create client modal start -->
+            <!-- Create client modal start -->
             <div class="modal" id="newProfile">
-                <div class="modal-dialog">
-                    <div class="modal-content">
+                <div class="modal-dialog modal-xl">
+                    <div class="modal-content modal-xl">
                         <!-- Modal Header -->
                         <div class="modal-header">
-                            <h4 class="modal-title">New Profile</h4>
+                            <h4 class="modal-title">Results for "@{{ queryString }}"</h4>
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
                         </div>
 
                         <!-- Modal body -->
                         <div class="modal-body">
+                            <div id="scope" class="row">
+                                <div v-show="loaded" class="loader"></div>
+
+                                <div class="card col-md-4" v-for="item in items.results">
+                                    <img class="card-img-top" :src="imageUrl + item.poster_path" alt="">
+                                    <a href="#" class="btn btn-primary">Watchlater</a>
+                                    <div class="card-body">
+                                        <h5 class="card-title">@{{ item.original_title }}</h5>
+                                        <p class="card-text">@{{ item.overview }}</p>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
 
                         <!-- Modal footer -->
@@ -54,18 +67,30 @@
             data:    {
                 profileName:     '',
                 queryString:'',
+                loaded:   true,
+                items:    [],
+                imageUrl: 'https://image.tmdb.org/t/p/w342',
+
+
             },
             methods: {
                 searchTMDB: function () {
                     $('#newProfile').modal('show');
-                    let url = 'https://api.themoviedb.org/3/search/movie?api_key='+ '{{env('TMDB_API_KEY')}}' + '&query=' + this.queryString;
+                    let vm = this;
+                    let url = 'https://api.themoviedb.org/3/search/movie?api_key='+ '{{env('TMDB_API_KEY')}}' + '&language=en-US&query=' + this.queryString + '&include_adult=true';
                     console.log(url);
-                    axios.get(url, {
-                    }).then(function (response) {
-                        console.log(response.data.results);
-                    }).catch(function (error) {
-                        alert('error with axios');
-                    });
+
+                    // make the request
+                    axios.get(url)
+                        .then(function (response) {
+                            // handle success
+                            vm.items = response.data;
+                            vm.loaded = false;
+                        })
+                        .catch(function (error) {
+                            // handle error
+                            console.log(error);
+                        });
                 },
             },
         });
