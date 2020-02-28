ff58d9faeb61a1df831dabd2093b611c0886efac
Luis Alberto Luft <luberluft@gmail.com>
2020-02-28 11:23:18 -0400

    Fix logo not appearing

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <img class="img-responsive" src="{{asset('storage/tmdb-logo.png')}}" alt="tmdb-logo.png">
+                    <img class="img-responsive" src="https://www.themoviedb.org/assets/2/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png" alt="tmdb-logo.png">
                 </div>
 
             </div>
